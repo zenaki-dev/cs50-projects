18735b6e206d1143afeda5952e0860e8defb702e
zen <117701512+zenaki-dev@users.noreply.github.com>
2023-02-14 21:32:43 -0500

    meow.c
    from cs50

---
+++ meow.c
@@ -0,0 +1,17 @@
+/Libaries | will need to run
+#include <stdio.h>
+
+//Custom Variables
+void meow(void)
+{
+    printf("meow\n");
+}
+
+//Integers
+int main(void)
+{
+    for (int i = 0; i < 3; i++)
+    {
+       meow();
+    }
+}
